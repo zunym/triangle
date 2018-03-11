3020bc3023741897d924d11ccb3211bb82aaad15
zunyinmon <zunyinmon@zunyinmon.com>
2018-03-11 23:39:32 +0800

    added features

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -63,20 +63,51 @@ jQuery(function($) {'use strict';
 		$('.field-toggle').fadeToggle(200);
 	});
 
+
+	// Subcribe
+	$('#form_subcribe').submit(function(){
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'POST',
+      data : $(this).serialize(),
+
+      success: function(){
+        console.log('form submitted.');  
+        alert('Thank you for subcribe us. We will keep in touch you.');
+      }
+    });
+    return false;
+});
+
 	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		});
-	});
+	$('#form').submit(function(){
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'POST',
+      data : $(this).serialize(),
+
+      success: function(){
+        console.log('form submitted.');  
+        alert('Your Message successfully sent, Thank you for contact us. As early as possible  we will contact you.');
+      }
+    });
+    return false;
+});
+	// var form = $('#main-contact-form');
+	// form.submit(function(event){
+	// 	event.preventDefault();
+
+	// 	var form_status = $('<div class="form_status"></div>');
+		
+	// 	$.ajax({
+	// 		url: $(this).attr('action'),
+	// 		beforeSend: function(){
+	// 			form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+	// 		}
+	// 	}).done(function(data){
+	// 		form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+	// 	});
+	// });
 
 	// Progress Bar
 	$.each($('div.progress-bar'),function(){
